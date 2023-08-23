dba6539f1ffbc247ce0ce94da497d3b02b2f1a9c
Avi Kaufman <avrham.kaufman@post.idc.ac.il>
2023-08-23 19:05:05 +0300

    first commit

---
+++ hw_2/C23 Ex02 Avi 205462591 Yuval 206094310/guess/Guess.cs
@@ -0,0 +1,23 @@
+﻿namespace Guess
+{
+    public class Guess
+    {
+        private char[] m_PlayerGuess = new char[4];
+
+        public Guess(string i_PlayerGuess)
+        {
+            if(i_PlayerGuess.Length == 4)
+            {
+                m_PlayerGuess[0] = i_PlayerGuess[0];
+                m_PlayerGuess[1] = i_PlayerGuess[1];
+                m_PlayerGuess[2] = i_PlayerGuess[2];
+                m_PlayerGuess[3] = i_PlayerGuess[3];
+            }
+        }
+
+        public char[] Get()
+        {
+            return m_PlayerGuess;
+        }
+    }
+}
